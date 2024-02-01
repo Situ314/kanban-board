16365a1e8fe853757daff2ad5a63fa5702b3a18b
carteaga <carteaga@pragmainvest.com.bo>
2024-02-01 16:10:31 -0300

    Fix Home view

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,19 +18,22 @@ class DatabaseSeeder extends Seeder
          \App\Models\User::factory()->create([
              'name' => 'admin',
              'email' => 'admin@kanban.com',
-             'password' => 'admin'
+             'password' => 'admin',
+             'email_verified_at' => now(),
          ]);
 
         \App\Models\User::factory()->create([
             'name' => 'user1',
             'email' => 'user1@kanban.com',
-            'password' => 'user123'
+            'password' => 'user123',
+            'email_verified_at' => now(),
         ]);
 
         \App\Models\User::factory()->create([
             'name' => 'user2',
             'email' => 'user2@example.com',
-            'password' => 'user234'
+            'password' => 'user234',
+            'email_verified_at' => now(),
         ]);
 
         Board::factory(3)->create();
